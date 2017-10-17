0d6529659b99c7e2f98b2303b62d173129c9f359
Rajeev Sukumaran <rs501@yahoo.com>
2017-10-17 17:12:45 -0400

    Week2 Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# These functions define global functions that take a matrix as well as
+# store the inverse of the matrix. It also has four functions that 
+# get and set the primary matrix as well as its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+makeCacheMatrix <- function(globalx = matrix()) {
+    globalm <- NULL
+    set <- function(localy) {
+        globalx <<- localy
+        globalm <<- NULL
+    }
+    get <- function() {
+        globalx
+    }
+    setinverse <- function(inversedmatrix) {
+        globalm <<- inversedmatrix
+    }
+    getinverse <- function() {
+        globalm
+    }
+    list(setnormal = set, getnormal = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+  
+## Return a matrix that is the inverse of 'x'
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# This function takes a matrix defined globally and either calculates
+# the inverse or if previously calculated, then returns if from stored cache
+
+cacheSolve <- function(globalx, ...) {
+    globalm <- globalx$getinverse()
+    if(!is.null(globalm)) {
+        message("getting cached data")
+        return(globalm)
+    }
+    localdata <- globalx$getnormal()
+    globalm <- solve(localdata, ...)
+    globalx$setinverse(globalm)
+    globalm
 }
